a3ea9cbd90bbf828209f8f7ef334dfa0eae951c4
Grenish rai <ariestheracer@gmail.com>
2024-02-11 21:15:16 +0530

    Add new assets for cat animation

---
+++ client/src/assets/index.js
@@ -0,0 +1,9 @@
+import catback from "./catback.svg";
+import animcat from "./cat.json";
+import cat from "./cat.svg";
+import animcat2 from "./cute-cat.json";
+import bomb from "./explode.svg";
+import diffuse from "./diffuse.svg";
+import shuffle from "./shuffle.svg";
+
+export { catback, animcat, cat, animcat2, bomb, diffuse, shuffle };
