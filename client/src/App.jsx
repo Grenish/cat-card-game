a4ae020d81ce1f591c40de287929fa5ce38f4580
Grenish rai <ariestheracer@gmail.com>
2024-02-11 21:17:56 +0530

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Game, Start } from "./components";
+
+const App = () => {
+  const [name, setName] = useState(null);
+
+  const handleStartGame = (name) => {
+    setName(name);
+  };
+
+  return (
+    <>{name ? <Game name={name} /> : <Start onStart={handleStartGame} />}</>
+  );
+};
+
+export default App;
